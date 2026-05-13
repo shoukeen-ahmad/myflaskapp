84a24b83a752df7b2a73c73c346640b49be3caa3
shoukeen ahmad <58034880+shoukeen-ahmad@users.noreply.github.com>
2026-05-13 17:56:27 +0530

    Update home route response message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(name)
 
 @app.route("/")
 def home():
-    return "Hello, This is Sam!"
+    return "Hello, This is Sam and another trigger!"
 
 if name == "main":
     app.run(host="0.0.0.0", port=5000)
